85c1c85b7d61f63dff9abd532649ff456997d25e
JuliaPiassentini <mariajuliapiassentini@gmail.com>
2023-11-30 19:40:59 -0300

    eliminacion carpeta script

--- js/glide.js
+++ js/glide.js
@@ -2,8 +2,8 @@ new Glide('.glide',{
     type: 'carousel',
     startAt:0,
     perView: 3,
-    gap: 30,
-    breakpoints:{
+    gap: 30 ,
+    breakpoint:{
         991:{
             perView: 2
         },
